b6610d0aea9b89ace510e0d5441bead59ebc7af5
Brent C. Wilson <brent@vectormediagroup.com>
2019-12-23 20:09:31 -0600

    Add date_is_today.

--- pi.pocketwatch.php
+++ pi.pocketwatch.php
@@ -31,13 +31,100 @@ class Pocketwatch {
         return ($format) ? $now->format($format) : $now->getTimestamp();
     }
 
+    public function date_is_today()
+    {
+        // Date is required.
+        $date = ee()->TMPL->fetch_param('date');
+        if (! $date)
+        {
+            return $this->noResults();
+        }
 
-// ----------------------------------------
-//  Plugin Usage
-// ----------------------------------------
+        // Default input format is Ymd.
+        $format = ee()->TMPL->fetch_param('format', 'Ymd');
+        // The string "timestamp" is a special case.
+        if (strtolower($format) == 'timestamp')
+        {
+            $format = 'U';
+        }
 
-// This function describes how the plugin is used.
-//  Make sure and use output buffering
+        $now = ee()->localize->format_date("%Y%m%d");
+        if ($format == 'Ymd')
+        {
+            $then = $date;
+        }
+        else
+        {
+            try {
+                $then = DateTime::createFromFormat($format, $date);
+                // If $then === false, creating the date failed.
+                if ($then === false)
+                {
+                    return $this->noResults();
+                }
+
+                $then = $then->format('Ymd');
+            } catch (Exception $e) {
+                return $this->noResults();
+            }
+        }
+
+        // If we have a tag pair, output the innards or no_results content, as appropriate.
+        if (ee()->TMPL->tagdata)
+        {
+            return ($now == $then) ? ee()->TMPL->tagdata : ee()->TMPL->no_results();
+        }
+        // Otherwise output true/false.
+        else
+        {
+            return ($now == $then);
+        }
+    }
+
+    public function date_is_this_week()
+    {
+
+    }
+
+    public function date_is_last_week()
+    {
+
+    }
+
+    public function date_is_next_week()
+    {
+
+    }
+
+    public function date_is_this_month()
+    {
+
+    }
+
+    public function date_is_this_year()
+    {
+
+    }
+
+    private function log($message)
+    {
+        ee()->load->library('logger');
+
+        // Only log once per day.
+        ee()->logger->developer($message, false, 86400);
+    }
+
+    private function noResults($noTagDataOutput = false)
+    {
+        return (ee()->TMPL->tagdata) ? ee()->TMPL->no_results() : $noTagDataOutput;
+    }
+
+    // ----------------------------------------
+    //  Plugin Usage
+    // ----------------------------------------
+
+    // This function describes how the plugin is used.
+    //  Make sure and use output buffering.
 
     static public function usage() {
         return;
