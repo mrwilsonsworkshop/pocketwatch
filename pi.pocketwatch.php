1d663769a9b4d8f2f8435d24d0ef3c773fd907bd
Brent C. Wilson <brent@vectormediagroup.com>
2019-12-24 15:40:56 -0600

    Add date_is_this/next/last_week functions.

--- pi.pocketwatch.php
+++ pi.pocketwatch.php
@@ -33,41 +33,17 @@ class Pocketwatch {
 
     public function date_is_today()
     {
-        // Date is required.
-        $date = ee()->TMPL->fetch_param('date');
-        if (! $date)
+        $then = $this->getDateTimeFromTagParameters('date', 'format');
+
+        // If $then is false, something went wrong while getting the datetime object.
+        if ($then === false)
         {
-            return $this->noResults();
+            return ee()->TMPL->no_results();
         }
 
-        // Default input format is Ymd.
-        $format = ee()->TMPL->fetch_param('format', 'Ymd');
-        // The string "timestamp" is a special case.
-        if (strtolower($format) == 'timestamp')
-        {
-            $format = 'U';
-        }
-
+        // Prep our comparison.
         $now = ee()->localize->format_date("%Y%m%d");
-        if ($format == 'Ymd')
-        {
-            $then = $date;
-        }
-        else
-        {
-            try {
-                $then = DateTime::createFromFormat($format, $date);
-                // If $then === false, creating the date failed.
-                if ($then === false)
-                {
-                    return $this->noResults();
-                }
-
-                $then = $then->format('Ymd');
-            } catch (Exception $e) {
-                return $this->noResults();
-            }
-        }
+        $then = $then->format('Ymd');
 
         // If we have a tag pair, output the innards or no_results content, as appropriate.
         if (ee()->TMPL->tagdata)
@@ -83,17 +59,108 @@ class Pocketwatch {
 
     public function date_is_this_week()
     {
+        $now = ee()->localize->format_date("%Y%W");
+        $then = $this->getDateTimeFromTagParameters('date', 'format');
 
+        // If $then is false, something went wrong while getting the datetime object.
+        if ($then === false)
+        {
+            return ee()->TMPL->no_results();
+        }
+
+        // Prep our comparison. We use the ISO-8601 definition of week number, which begins on Monday.
+        // ...Unless the user wants the week to start on Sunday. Then we just need to add one day.
+        if (strtolower(ee()->TMPL->fetch_param('week_start_day')) == 'sunday')
+        {
+            $then = $then->add(DateInterval::createFromDateString('1 day'))->format('oW');
+        }
+        else
+        {
+            $then = $then->format('oW');
+        }
+
+        // If we have a tag pair, output the innards or no_results content, as appropriate.
+        if (ee()->TMPL->tagdata)
+        {
+            return ($now == $then) ? ee()->TMPL->tagdata : ee()->TMPL->no_results();
+        }
+        // Otherwise output true/false.
+        else
+        {
+            return ($now == $then);
+        }
     }
 
     public function date_is_last_week()
     {
+        $now = new DateTime();
+        $now = $now->setTimestamp(ee()->localize->now)->sub(DateInterval::createFromDateString('7 days'))->format('oW');
 
+        $then = $this->getDateTimeFromTagParameters('date', 'format');
+
+        // If $then is false, something went wrong while getting the datetime object.
+        if ($then === false)
+        {
+            return ee()->TMPL->no_results();
+        }
+
+        // Prep our comparison. We use the ISO-8601 definition of week number, which begins on Monday.
+        // ...Unless the user wants the week to start on Sunday. Then we just need to add one day.
+        if (strtolower(ee()->TMPL->fetch_param('week_start_day')) == 'sunday')
+        {
+            $then = $then->add(DateInterval::createFromDateString('1 day'))->format('oW');
+        }
+        else
+        {
+            $then = $then->format('oW');
+        }
+
+        // If we have a tag pair, output the innards or no_results content, as appropriate.
+        if (ee()->TMPL->tagdata)
+        {
+            return ($now == $then) ? ee()->TMPL->tagdata : ee()->TMPL->no_results();
+        }
+        // Otherwise output true/false.
+        else
+        {
+            return ($now == $then);
+        }
     }
 
     public function date_is_next_week()
     {
+        $now = new DateTime();
+        $now = $now->setTimestamp(ee()->localize->now)->add(DateInterval::createFromDateString('7 days'))->format('oW');
 
+        $then = $this->getDateTimeFromTagParameters('date', 'format');
+
+        // If $then is false, something went wrong while getting the datetime object.
+        if ($then === false)
+        {
+            return ee()->TMPL->no_results();
+        }
+
+        // Prep our comparison. We use the ISO-8601 definition of week number, which begins on Monday.
+        // ...Unless the user wants the week to start on Sunday. Then we just need to add one day.
+        if (strtolower(ee()->TMPL->fetch_param('week_start_day')) == 'sunday')
+        {
+            $then = $then->add(DateInterval::createFromDateString('1 day'))->format('oW');
+        }
+        else
+        {
+            $then = $then->format('oW');
+        }
+
+        // If we have a tag pair, output the innards or no_results content, as appropriate.
+        if (ee()->TMPL->tagdata)
+        {
+            return ($now == $then) ? ee()->TMPL->tagdata : ee()->TMPL->no_results();
+        }
+        // Otherwise output true/false.
+        else
+        {
+            return ($now == $then);
+        }
     }
 
     public function date_is_this_month()
@@ -104,6 +171,32 @@ class Pocketwatch {
     public function date_is_this_year()
     {
 
+    }
+
+    private function getDateTimeFromTagParameters($dateParamName = 'date', $formatParamName = 'format')
+    {
+        // Date is required.
+        $date = ee()->TMPL->fetch_param($dateParamName);
+        if (! $date)
+        {
+            return false;
+        }
+
+        // Default input format is Ymd.
+        $format = ee()->TMPL->fetch_param($formatParamName, 'Ymd');
+        // The string "timestamp" is a special case.
+        if (strtolower($format) == 'timestamp')
+        {
+            $format = 'U';
+        }
+
+        try {
+            $date = DateTime::createFromFormat($format, $date);
+        } catch (Exception $e) {
+            return false;
+        }
+
+        return $date;
     }
 
     private function log($message)
